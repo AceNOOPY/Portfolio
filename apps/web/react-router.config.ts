949f411d9b306466163ed54e6935068bd03dec30
Adrian Cristopher Villamor <39563610+AceNOOPY@users.noreply.github.com>
2026-04-06 05:01:10 +0000

    Fix React Router config import issue

--- apps/web/react-router.config.ts
+++ apps/web/react-router.config.ts
@@ -1,7 +1,6 @@
 import type { Config } from '@react-router/dev/config';
 
 export default {
-	appDirectory: './src/app',
-	ssr: false, // Disable SSR for static GitHub Pages deployment
-	prerender: ['/'],
+  appDirectory: './src/app',
+  ssr: false,
 } satisfies Config;
